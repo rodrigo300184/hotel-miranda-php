3ca6e58494d619ab71c0ada563505948f3bba055
rodrigo300184 <rodrigo300184@gmail.com>
2023-11-30 11:56:26 +0100

    Server connection to .env

--- setup.php
+++ setup.php
@@ -7,22 +7,31 @@ use eftec\bladeone\BladeOne;
 $views = __DIR__ . '/views';
 $cache = __DIR__ . '/cache';
 
-$blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
+$blade = new BladeOne($views, $cache, BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
 
 // Server Connection
 
- $servername = "127.0.0.1:33061";
- $username = "root";
- $password = "mypassword";
- $database = "prueba_mysql_api_miranda";
+$envFile = __DIR__ . '/.env';
+
+if (file_exists($envFile)) {
+  $envVariables = parse_ini_file($envFile);
+
+  foreach ($envVariables as $key => $value) {
+    $_ENV[$key] = $value;
+    putenv("$key=$value");
+  }
+}
+
+$server_name = $_ENV['SERVER_NAME'];
+$username = $_ENV['USERNAME'];
+$password = $_ENV['PASSWORD'];
+$database = $_ENV['DATABASE'];
+
 
 // Create connection
- $conn = new mysqli($servername, $username, $password,$database);
+$conn = new mysqli($servername, $username, $password, $database);
 
 //Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-?>
-
