d6430828826af9e93b2591071cd49fcd6f407520
rodrigo300184 <rodrigo300184@gmail.com>
2023-12-05 16:39:32 +0100

    Footer to desktop

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -52,61 +52,69 @@
     </div>
     <!-- Footer Section -->
     <section class="footer">
-        <div class="footer-content">
-            <div class="footer-letterhead">H</div>
-            <h2 class="footer-letterhead-text"><span>HOTEL </span>MIRANDA</h2>
-            <p class="footer-paragraph">
-                Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed doing
-                eius mod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-                ad minim veniam, quis nostrud exercitat ion ullamco laboris nisi.
-            </p>
-            <div class="footer-socials">
-                <a href="https://www.facebook.com/" class="footer-socials-facebook"></a>
-                <a href="https://twitter.com/" class="footer-socials-twitter"></a>
-                <a href="https://www.behance.net/" class="footer-socials-behace"></a>
-                <a href="https://www.linkedin.com/" class="footer-socials-linkedin"></a>
-                <a href="https://www.youtube.com/" class="footer-socials-youtube"></a>
-            </div>
-            <h4 class="footer-subtitle">Services.</h4>
-            <div class="footer-services">
-                <ul>
-                    <li>+ Resturant & Bar</li>
-                    <li>+ Swimming Pool</li>
-                    <li>+ Wellness & Spa</li>
-                    <li>+ Restaurant</li>
-                    <li>+ Conference Room</li>
-                    <li>+ Coctail Party House</li>
-                </ul>
-                <ul>
-                    <li>+ Gaming Zone</li>
-                    <li>+ Marrige Party</li>
-                    <li>+ Party Planning</li>
-                    <li>+ Tour Consultancy</li>
-                </ul>
-            </div>
-            <h4 class="footer-subtitle footer-subtitle-contact">Contact Us.</h4>
-            <div class="footer-contact-data">
-                <div class="footer-contact">
-                    <img class="footer-contact-icons" src="./resources/icons/footer/telephone.svg" alt="" />
-                    <div>
-                        <h4 class="footer-contact-title">Phone Number</h4>
-                        <p class="footer-contact-subtitle">+347 876 765 76 577</p>
+        <div class="footer-inner">
+            <div class="footer-content">
+                <div class="footer-container_first">
+                    <div class="footer-letterhead">H</div>
+                    <h2 class="footer-letterhead-text"><span>HOTEL </span>MIRANDA</h2>
+                    <p class="footer-paragraph">
+                        Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed doing
+                        eius mod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                        ad minim veniam, quis nostrud exercitat ion ullamco laboris nisi.
+                    </p>
+                    <div class="footer-socials">
+                        <a href="https://www.facebook.com/" class="footer-socials-facebook"></a>
+                        <a href="https://twitter.com/" class="footer-socials-twitter"></a>
+                        <a href="https://www.behance.net/" class="footer-socials-behace"></a>
+                        <a href="https://www.linkedin.com/" class="footer-socials-linkedin"></a>
+                        <a href="https://www.youtube.com/" class="footer-socials-youtube"></a>
                     </div>
                 </div>
-                <div class="footer-contact">
-                    <img class="footer-contact-icons" src="./resources/icons/footer/email.svg" alt="" />
-                    <div>
-                        <h4 class="footer-contact-title">Email</h4>
-                        <p class="footer-contact-subtitle">
-                            rodrigomartinez.correo@gmail.com
-                        </p>
+                <div class="footer-container_second">
+                    <h4 class="footer-subtitle">Services.</h4>
+                    <div class="footer-services">
+                        <ul>
+                            <li>+ Resturant & Bar</li>
+                            <li>+ Swimming Pool</li>
+                            <li>+ Wellness & Spa</li>
+                            <li>+ Restaurant</li>
+                            <li>+ Conference Room</li>
+                            <li>+ Coctail Party House</li>
+                        </ul>
+                        <ul>
+                            <li>+ Gaming Zone</li>
+                            <li>+ Marrige Party</li>
+                            <li>+ Party Planning</li>
+                            <li>+ Tour Consultancy</li>
+                        </ul>
                     </div>
                 </div>
-                <div class="footer-contact">
-                    <img class="footer-contact-icons" src="./resources/icons/footer/location.svg" alt="" />
-                    <div>
-                        <h4 class="footer-contact-title">Location</h4>
-                        <p class="footer-contact-subtitle">Málaga - Spain</p>
+                <div class="footer-container_third">
+                    <h4 class="footer-subtitle footer-subtitle-contact">Contact Us.</h4>
+                    <div class="footer-contact-data">
+                        <div class="footer-contact">
+                            <img class="footer-contact-icons" src="./resources/icons/footer/telephone.svg" alt="" />
+                            <div>
+                                <h4 class="footer-contact-title">Phone Number</h4>
+                                <p class="footer-contact-subtitle">+347 876 765 76 577</p>
+                            </div>
+                        </div>
+                        <div class="footer-contact">
+                            <img class="footer-contact-icons" src="./resources/icons/footer/email.svg" alt="" />
+                            <div>
+                                <h4 class="footer-contact-title">Email</h4>
+                                <p class="footer-contact-subtitle">
+                                    rodrigomartinez.correo@gmail.com
+                                </p>
+                            </div>
+                        </div>
+                        <div class="footer-contact">
+                            <img class="footer-contact-icons" src="./resources/icons/footer/location.svg" alt="" />
+                            <div>
+                                <h4 class="footer-contact-title">Location</h4>
+                                <p class="footer-contact-subtitle">Málaga - Spain</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -122,7 +130,7 @@
     <script>
         Toastify({
             text: "{{$notification['message']}}",
-            //duration: 3000,
+            duration: 5000,
             newWindow: true,
             close: true,
             gravity: "top",
