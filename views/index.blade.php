7dc51dc4975ad0d7b982f09f2b956e4391b10a58
rodrigo300184 <rodrigo300184@gmail.com>
2026-04-09 15:35:01 +0200

    update banner main title to enhance user experience

--- views/index.blade.php
+++ views/index.blade.php
@@ -7,7 +7,7 @@
 <section class="banner">
   <h2 class="banner-subtitle subtitle">THE ULTIMATE LUXURY EXPERIENCE</h2>
   <h1 class="banner-main-title main-title main-title-white main-title main-title-white--big">
-    Probando 1,2,3
+    The Perfect Base For You
   </h1>
   <div class="banner_button-container">
     <a href="/rooms.php" class="button button_small button_small--gold">
